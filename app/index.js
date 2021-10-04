4440cef56750a68f5cad2937df0fa177470eb54b
Imagine Rabbits <keerati.jear@gmail.com>
2021-10-04 20:38:26 +0700

    Add telegram bot

--- app/index.js
+++ app/index.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(`${process.env.BOT_ACCESS_TOKEN}`);
+
 const express = require('express')
 const dbConnection = require('../infra/database')
 const app = express()
@@ -14,8 +19,11 @@ function timestamp() {
     return new Date()
 }
 
-app.get(baseVersion + '/watering/value/:value', function (req, res) {
-    const data = { timestamp: timestamp(), moisture: parseInt(req.params.value) }
+app.get(baseVersion + '/watering/value/:value', async function (req, res) {
+    const moisture = parseInt(req.params.value)
+    const data = { timestamp: timestamp(), moisture }
+
+    sendAlert(`[Alert] Watering ☀️ !`, moisture)
 
     dbConnection.query(`INSERT INTO watering set ?`, data, function (err, result) {
         if (err) throw err;
@@ -25,8 +33,11 @@ app.get(baseVersion + '/watering/value/:value', function (req, res) {
     });
 })
 
-app.get(baseVersion + '/moisture/value/:value', function (req, res) {
-    const data = { timestamp: timestamp(), moisture: parseInt(req.params.value) }
+app.get(baseVersion + '/moisture/value/:value', async function (req, res) {
+    const moisture = parseInt(req.params.value)
+    const data = { timestamp: timestamp(), moisture }
+
+    sendAlert(`[Alert] Current Moisture 🌱 `, moisture)
 
     dbConnection.query(`INSERT INTO moisture set ?`, data, function (err, result) {
         if (err) throw err;
@@ -34,6 +45,30 @@ app.get(baseVersion + '/moisture/value/:value', function (req, res) {
         res.send(200, data)
     });
 })
+
+async function sendAlert(message, moisture) {
+    if (moisture > 50) {
+        return
+    }
+
+    bot.telegram.sendMessage(process.env.CHAT_ID, `${message} ( Moisture : ${moisture} )`, {})
+}
+
+bot.command('current', ctx => {
+    dbConnection.query(`SELECT timestamp, moisture FROM moisture ORDER BY timestamp desc limit 1`, function (err, result) {
+        if (err) throw err;
+        console.log("1 record get");
+
+        if (result && result.length > 0) {
+            const { timestamp, moisture } = result[0]
+            bot.telegram.sendMessage(process.env.CHAT_ID, `⏲️ Timestamp: ${new Date(timestamp).toLocaleString()}  \n ☘️ Moisture : ${moisture}`, {})
+        } else {
+            bot.telegram.sendMessage(process.env.CHAT_ID, `🔥 No current value`, {})
+        }
+    });
+})
+
+bot.launch();
 
 app.listen(process.env.PORT, () => {
     console.log(`Start App on Port: ${process.env.PORT}`)
